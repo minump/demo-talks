efa0f74b03bf6a4e7798555566593b32381e95b3
Sanketh Katta <sanketh@smartcar.com>
2018-11-27 11:53:17 -0800

    Clean up python code and requirements.

--- python/app.py
+++ python/app.py
@@ -4,33 +4,41 @@ CLIENT_SECRET = ''
 import smartcar
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
-
+# 1. Create an instance of Smartcar's client.
 client = smartcar.AuthClient(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     redirect_uri='http://localhost:8000/callback',
-    scope=['read_vehicle_info', 'read_location', 'read_odometer']
+    scope=['read_vehicle_info', 'read_location', 'control_security']
 )
 
+# 2. Create a new webserver with the Flask framework.
+app = Flask(__name__)
+
+# 3. Create a page with a 'Connect Car' button.
 @app.route('/', methods=['GET'])
 def index():
     auth_url = client.get_auth_url(force=True)
     return '''
-        <h1>Hello, Hackbright!</h1>
+        <h1>Hello, World!</h1>jjj
         <a href=%s>
           <button>Connect Car</button>
         </a>
     ''' % auth_url
 
+# 4. On an HTTP GET to our callback will exchange our OAuth Auth Code
+#    for an Access Token and log it out.
 @app.route('/callback', methods=['GET'])
 def callback():
     code = request.args.get('code')
     access = client.exchange_code(code)
-    
+
+    # Log the access token response
     print(access)
 
+    # Respond with a success status to browser
     return jsonify(access)
 
+# 5. Let's start up the server at port 8000.
 if __name__ == '__main__':
     app.run(port=8000)
